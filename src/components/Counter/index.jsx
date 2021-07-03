f17f86a0c0ade9ecabf11e41b3bb4651728576e4
Viktoriia <pecourses@gmail.com>
2021-07-03 17:17:08 +0300

    add changes in Counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -4,29 +4,36 @@ class Counter extends Component {
   //rconst
   constructor(props) {
     super(props);
-
+    // иммутабельность - сост не изменяется, а заменяется
     this.state = {
       counter: 0,
+      step: 1,
     };
     // this.increment = this.increment.bind(this);
   }
 
   decrement = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter - 1 });
+    const { counter, step } = this.state;
+    this.setState({ counter: counter - step }); // +rerender
   };
 
   increment = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+    const { counter, step } = this.state;
+    this.setState({ counter: counter + step }); // +rerender
+  };
+
+  changeStep = e => {
+    this.setState({ step: Number(e.target.value) });
   };
 
   render() {
-    const { counter } = this.state;
+    const { counter, step } = this.state;
 
     return (
       <>
         <div>{counter}</div>
+        <input type="number" value={step} onChange={this.changeStep} />
+        <br />
         <button onClick={this.decrement}>-</button>
         <button onClick={this.increment}>+</button>
       </>
