dd1f8f897ddc914bd50cc40b5140327455291916
Viktoriia <pecourses@gmail.com>
2021-07-10 16:22:38 +0300

    add validation to LoginForm

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -7,22 +7,26 @@ class LoginForm extends Component {
 
     this.state = {
       loginValue: '',
+      isLoginValid: false,
       passwValue: '',
     };
   }
 
-  // handleLoginChange = ({ target: { value } }) => {
-  //   // const {target:{value}} = e
-  //   this.setState({ loginValue: value });
-  // };
-
-  // handlePasswordChange = ({ target: { value } }) => {
-  //   this.setState({ passwValue: value });
-  // };
-
-  handleChange = ({ target: { value, name } }) => {
-    this.setState({ [name]: value });
+  handleLoginChange = ({ target: { value } }) => {
+    // const {target:{value}} = e
+    this.setState({
+      loginValue: value,
+      isLoginValid: /^\S+$/.test(value),
+    });
   };
+
+  handlePasswordChange = ({ target: { value } }) => {
+    this.setState({ passwValue: value });
+  };
+
+  // handleChange = ({ target: { value, name } }) => {
+  //   this.setState({ [name]: value });
+  // };
 
   submitHandler = e => {
     e.preventDefault();
@@ -39,7 +43,7 @@ class LoginForm extends Component {
             placeholder="login"
             name="loginValue"
             value={loginValue}
-            onChange={this.handleChange}
+            onChange={this.handleLoginChange}
           />
         </label>
         <label>
@@ -49,7 +53,7 @@ class LoginForm extends Component {
             name="passwValue"
             placeholder="password"
             value={passwValue}
-            onChange={this.handleChange}
+            onChange={this.handlePasswordChange}
           />
         </label>
         <button type="submit">Login</button>
