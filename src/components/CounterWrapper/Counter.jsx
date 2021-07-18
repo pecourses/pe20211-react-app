1d80fcc5baa38ca5d88539446402874fae4f5e97
Viktoriia <pecourses@gmail.com>
2021-07-18 19:32:03 +0300

    add PureComponent

--- src/components/CounterWrapper/Counter.jsx
+++ src/components/CounterWrapper/Counter.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 /* написать counter:
  выводить значение (state)
  по нажатию на -/+ уменьшать/увеличивать значение*/
-class Counter extends Component {
+class Counter extends PureComponent {
   constructor(props) {
     super(props);
 
@@ -11,6 +11,15 @@ class Counter extends Component {
       count: 0,
     };
   }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return this.props.step === nextProps.step;
+  // }
+
+  // PureComponent:
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (this.props.step !== nextProps.step || this.state.count !== nextState.count);
+  // }
 
   decrement = () => {
     this.setState({ count: this.state.count - this.props.step });
@@ -28,8 +37,32 @@ class Counter extends Component {
         <div>{count}</div>
         <button onClick={this.decrement}>-</button>
         <button onClick={this.increment}>+</button>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count });
+          }}>
+          0
+        </button>
       </>
     );
   }
 }
 export default Counter;
+
+//PureComponent
+//shouldComponentUpdate:
+// производит поверхностное сравнение props/state
+// state = {
+//   count: 0,
+//   value: {
+//     step: 1,
+//   },
+// };
+
+// newState = {
+//   count: 0,
+//   value: {
+//     step: 1,
+//   },
+// };
+// 1 значения свойств - примитивы
